4e48a847b2ee84afdcc7694a48f69eeca389f941
Jakub Farnik <yakubiq@proton.me>
2025-05-08 19:23:03 +0200

    lora refactoring

--- main/raketa.c
+++ main/raketa.c
@@ -12,6 +12,8 @@
 #include "driver/gpio.h"
 #include "i2c_bus.h"
 #include "driver/uart.h"
+
+
 #define I2C_MASTER_SDA_IO   (gpio_num_t)14       /*!< gpio number for I2C master data  */
 #define I2C_MASTER_SCL_IO   (gpio_num_t)15       /*!< gpio number for I2C master clock */
 #define I2C_MASTER_FREQ_HZ  100000               /*!< I2C master clock frequency */
@@ -38,18 +40,8 @@
 /*
  * Lora sx1278-smt definitions and functions
 */
-#include "sx127x.h"
-#define PIN_NUM_MISO 19
-#define PIN_NUM_MOSI 23
-#define PIN_NUM_CLK  18
-#define PIN_NUM_CS   5
-#define PIN_NUM_RST  14
-#define PIN_NUM_DIO0 26
 
-static struct sx127x_t *lora = NULL;
-
-/* @System declarations*/
-// TODO: Change data types to esp_err_t and ensure errors are handled properly
+/* @System structures*/
 struct BME_STRUCTURE{
     bme280_handle_t bmeHandle;
     float temperature;
@@ -71,68 +63,54 @@ struct MEASURING_MODULES{
 
 int systemInitializaton(struct MEASURING_MODULES* modules);
 
-/* @I2C Declarations*/
+/* @I2C */
 static i2c_bus_handle_t i2cBusHandle = NULL;
 i2c_bus_handle_t i2cInit();
 void updateBME(struct BME_STRUCTURE* bme);
 void updateMPU(struct MPU_STRUCTURE* mpu);
 
-/* @GPS Declarations */
+/* @GPS */
 void gpsInit(char* gpsBuffer);
 void gpsUpdate(char* gpsBuffer);
 
 
-// lora testing
-#include <driver/gpio.h>
+/* @LORA */
 #include <driver/spi_master.h>
 #include <esp_intr_alloc.h>
-#include <esp_log.h>
-#include <freertos/task.h>
 #include <sx127x.h>
-#include "misc/esp_utils.h"
+#include <driver/spi_common.h>
+#include <driver/spi_master.h>
 
-static const char *TAG = "sx127x";
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define DIO0 26
+#define RST 23
 
 sx127x device;
-int messages_sent = 0;
-int supported_power_levels[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20};
-int supported_power_levels_count = sizeof(supported_power_levels) / sizeof(int);
-int current_power_level = 0;
+int total_packets_received = 0;
+const UBaseType_t xArrayIndex = 0;
+TaskHandle_t handle_interrupt;
+static const char *TAG = "esp_utils";
+void (*global_tx_callback)(sx127x *device);
+// breakpoint
+int current_power_level = 11;
 
-void tx_callback(sx127x *device) {
-  if (messages_sent > 0) {
-    ESP_LOGI(TAG, "transmitted");
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-  if (messages_sent == 0) {
-    uint8_t data[] = {0xCA, 0xFE};
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission(data, sizeof(data), device));
-  } else if (messages_sent == 1) {
-    // 200 bytes
-    uint8_t data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7};
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission(data, sizeof(data), device));
-  } else if (messages_sent == 2) {
-    // 255 bytes
-    uint8_t data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, 0xdf, 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef, 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe};
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission(data, sizeof(data), device));
-  } else if (current_power_level < supported_power_levels_count) {
-    uint8_t data[] = {0xCA, 0xFE};
-    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission(data, sizeof(data), device));
-    ESP_ERROR_CHECK(sx127x_tx_set_pa_config(SX127x_PA_PIN_BOOST, supported_power_levels[current_power_level], device));
-    current_power_level++;
-  } else {
-    return;
-  }
-  ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_TX, SX127x_MODULATION_LORA, device));
-  ESP_LOGI(TAG, "transmitting");
-  messages_sent++;
-}
-
-
-// lora testing
+spi_device_handle_t loraInit();
+void loraTx(sx127x *device);
+void setup_gpio_interrupts(gpio_num_t gpio, sx127x *device, gpio_int_type_t type);
+void sx127x_reset();
+void sx127x_init_spi(spi_device_handle_t *handle);
+esp_err_t setup_task(sx127x *device);
+esp_err_t setup_tx_task(sx127x *device, void (*tx_callback)(sx127x *device));
+void handle_interrupt_task(void *arg);
+void IRAM_ATTR handle_interrupt_fromisr(void *arg);
+void handle_interrupt_tx_task(void *arg);
 
 
 static esp_err_t err;
+static const char *sxTag = "sx127x";
 void app_main(void){
     char* taskName = pcTaskGetName(NULL);
     struct MEASURING_MODULES modules = {};
@@ -158,18 +136,18 @@ void app_main(void){
     ESP_ERROR_CHECK(sx127x_lora_set_modem_config_2(SX127x_SF_9, &device));
     ESP_ERROR_CHECK(sx127x_lora_set_syncword(18, &device));
     ESP_ERROR_CHECK(sx127x_set_preamble_length(8, &device));
-    sx127x_tx_set_callback(tx_callback, &device);
+    sx127x_tx_set_callback(loraTx, &device);
 
     gpio_install_isr_service(0);
     setup_gpio_interrupts((gpio_num_t)DIO0, &device, GPIO_INTR_POSEDGE);
 
-    ESP_ERROR_CHECK(sx127x_tx_set_pa_config(SX127x_PA_PIN_BOOST, supported_power_levels[current_power_level], &device));
+    ESP_ERROR_CHECK(sx127x_tx_set_pa_config(SX127x_PA_PIN_BOOST, current_power_level, &device));
     sx127x_tx_header_t header = {
         .enable_crc = true,
         .coding_rate = SX127x_CR_4_5};
     ESP_ERROR_CHECK(sx127x_lora_tx_set_explicit_header(&header, &device));
 
-    ESP_ERROR_CHECK(setup_tx_task(&device, tx_callback));
+    ESP_ERROR_CHECK(setup_tx_task(&device, loraTx));
     while(1){
         updateBME(&modules.bme280);
         updateMPU(&modules.mpu6050);
@@ -187,8 +165,7 @@ void app_main(void){
                 modules.mpu6050.gyroscope.gyro_x,
                 modules.mpu6050.gyroscope.gyro_y,
                 modules.mpu6050.gyroscope.gyro_z);
-        gpsUpdate(modules.gpsBuffer);
-        ESP_LOGI(taskName, "GPS: %s", modules.gpsBuffer);
+        //ESP_LOGI(taskName, "GPS: %s", modules.gpsBuffer);
         vTaskDelay(seconds(1));
     }
 }
@@ -226,7 +203,6 @@ int systemInitializaton(struct MEASURING_MODULES* modules){
     TEST_ASSERT_EQUAL_MESSAGE(ESP_OK, err, "MPU6050 Waking up failed.");
 
     // >! GPS Initialization
-    gpsInit(modules->gpsBuffer);
     vTaskDelay(milliseconds(600));
 
     return 1;
@@ -278,49 +254,86 @@ void updateMPU(struct MPU_STRUCTURE* mpu){
     vTaskDelay(milliseconds(100));
 }
 
-void gpsInit(char* gpsBuffer){
-    uart_config_t uartConfig = {
-        .baud_rate= 9600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    };
-    ESP_ERROR_CHECK(uart_param_config(UART_NUM, &uartConfig));
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM, UART_PIN_NO_CHANGE, 12,
-                UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, BUFFER_LENGTH * 2,
-                0, 0, NULL, 0));
-    vTaskDelay(seconds(1));
+void loraTx(sx127x *device) {
+    // 255 bytes
+    uint8_t data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, 0xdf, 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef, 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe};
+    ESP_ERROR_CHECK(sx127x_lora_tx_set_for_transmission(data, sizeof(data), device));
+    ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_TX, SX127x_MODULATION_LORA, device));
+    ESP_LOGI(TAG, "transmitting");
 }
-void gpsUpdate(char* gpsBuffer) {
-    /*
-       memset(gpsBuffer, 0, BUFFER_LENGTH);
-       int idx = 0;
-       while (idx < BUFFER_LENGTH - 1) {
-       uint8_t byte;
-       int len = uart_read_bytes(UART_NUM, &byte, 1, pdMS_TO_TICKS(100));  // Wait 100ms for a byte
-       if (len > 0) {
-       gpsBuffer[idx++] = byte;
-       if (byte == '\n') {
-       gpsBuffer[idx] = '\0';
-       return;
-       }
-       } else {
-       break;
-       }
-       }
-       strcpy(gpsBuffer, "No full GPS sentence received.");
-       */
-    memset(gpsBuffer, 0, BUFFER_LENGTH);
-    int idx = 0;
-    while (idx < BUFFER_LENGTH - 1) {
-        uint8_t byte;
-        uart_read_bytes(UART_NUM, &byte, 1, pdMS_TO_TICKS(100));  // Wait 100ms for a byte
-        gpsBuffer[idx++] = byte;
-        if (byte == '\n') {
-            gpsBuffer[idx] = '\0';
-        }
-    }
+void setup_gpio_interrupts(gpio_num_t gpio, sx127x *device, gpio_int_type_t type) {
+  ESP_ERROR_CHECK(gpio_set_direction(gpio, GPIO_MODE_INPUT));
+  ESP_ERROR_CHECK(gpio_pulldown_en(gpio));
+  ESP_ERROR_CHECK(gpio_pullup_dis(gpio));
+  ESP_ERROR_CHECK(gpio_set_intr_type(gpio, type));
+  ESP_ERROR_CHECK(gpio_isr_handler_add(gpio, handle_interrupt_fromisr, (void *) device));
 }
 
+esp_err_t setup_task(sx127x *device) {
+  BaseType_t task_code = xTaskCreatePinnedToCore(handle_interrupt_task, "handle interrupt", 8196, device, 2, &handle_interrupt, xPortGetCoreID());
+  if (task_code != pdPASS) {
+    ESP_LOGE(TAG, "can't create task %d", task_code);
+    return ESP_FAIL;
+  }
+  return ESP_OK;
+}
+
+esp_err_t setup_tx_task(sx127x *device, void (*tx_callback)(sx127x *device)) {
+  global_tx_callback = tx_callback;
+  BaseType_t task_code = xTaskCreatePinnedToCore(handle_interrupt_tx_task, "handle interrupt", 8196, device, 2, &handle_interrupt, xPortGetCoreID());
+  if (task_code != pdPASS) {
+    ESP_LOGE(TAG, "can't create task %d", task_code);
+    return ESP_FAIL;
+  }
+  return ESP_OK;
+}
+
+void sx127x_reset() {
+  ESP_ERROR_CHECK(gpio_set_direction((gpio_num_t) RST, GPIO_MODE_OUTPUT));
+  ESP_ERROR_CHECK(gpio_set_level((gpio_num_t) RST, 0));
+  vTaskDelay(pdMS_TO_TICKS(5));
+  ESP_ERROR_CHECK(gpio_set_level((gpio_num_t) RST, 1));
+  vTaskDelay(pdMS_TO_TICKS(10));
+  ESP_LOGI(TAG, "sx127x was reset");
+}
+
+void sx127x_init_spi(spi_device_handle_t *handle) {
+  spi_bus_config_t config = {
+      .mosi_io_num = MOSI,
+      .miso_io_num = MISO,
+      .sclk_io_num = SCK,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+      .max_transfer_sz = 0,
+  };
+  ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &config, 1));
+  spi_device_interface_config_t dev_cfg = {
+      .clock_speed_hz = 4E6,
+      .spics_io_num = SS,
+      .queue_size = 16,
+      .command_bits = 0,
+      .address_bits = 8,
+      .dummy_bits = 0,
+      .mode = 0};
+  ESP_ERROR_CHECK(spi_bus_add_device(SPI2_HOST, &dev_cfg, handle));
+}
+void handle_interrupt_task(void *arg) {
+  while (1) {
+    if (ulTaskNotifyTakeIndexed(xArrayIndex, pdTRUE, portMAX_DELAY) > 0) {
+      sx127x_handle_interrupt((sx127x *) arg);
+    }
+  }
+}
+void IRAM_ATTR handle_interrupt_fromisr(void *arg) {
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  vTaskNotifyGiveIndexedFromISR(handle_interrupt, xArrayIndex, &xHigherPriorityTaskWoken);
+  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+void handle_interrupt_tx_task(void *arg) {
+  global_tx_callback((sx127x *) arg);
+  while (1) {
+    if (ulTaskNotifyTakeIndexed(xArrayIndex, pdTRUE, portMAX_DELAY) > 0) {
+      sx127x_handle_interrupt((sx127x *) arg);
+    }
+  }
+}
